3c77f904c8e29176cfa03ebc3a0e83edaed1e7d1
allaramanaidu <ramanaidu.alla@gmail.com>
2018-02-22 17:43:55 +0530

    changing comments in return json.dumps{...}

---
+++ src/logger.py
@@ -0,0 +1,42 @@
+import logging
+import configparser
+
+class logger_class:
+    def __init__(self, name):
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
+        self.logtype = self.config['log']['level']
+        self.loglocation = str(self.config['log']['location'])
+        self.logger = logging.getLogger(name)
+        self.formater = logging.Formatter('[%(asctime)s] [%(filename)-12s] [%(levelname)-8s] [%(message)s]')
+
+        if self.loglocation == 'console':
+            self.consoleHandler = logging.StreamHandler()
+            self.consoleHandler.setFormatter(self.formater)
+            self.logger.addHandler(self.consoleHandler)
+            if self.logtype == 'INFO':
+                self.consoleHandler.setLevel(logging.INFO)
+            elif self.logtype == 'DEBUG':
+                self.consoleHandler.setLevel(logging.DEBUG)
+            elif self.logtype == 'ERROR':
+                self.consoleHandler.setLevel(logging.ERROR)
+            else :
+                pass
+
+
+        else  # save into file
+            self.fileHandler = logging.FileHandler('test.log')
+            self.fileHandler.setFormatter(self.formater)
+            self.logger.addHandler(self.fileHandler)
+            if self.logtype == 'INFO':
+                self.fileHandler.setLevel(logging.INFO)
+            elif self.logtype == 'DEBUG':
+                self.fileHandler.setLevel(logging.DEBUG)
+            elif self.logtype == 'ERROR':
+                self.fileHandler.setLevel(logging.ERROR)
+            else:
+                pass
+
+
+    def something(self):
+        pass
--- src/mysqlConnector.py
+++ src/mysqlConnector.py
@@ -3,7 +3,10 @@ from pandas import read_csv
 from sqlalchemy import create_engine
 import sys
 import configparser
+import logger
 
+
+loggerObject = logger.logger_class(__name__)
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -19,27 +22,35 @@ def to_mysql(filename, db_name):
     :param db_name:
     :return:
     """
-    global engine
-    tableName = filename[:-4]
-    engine.execute("CREATE DATABASE IF NOT EXISTS "+ db_name)  # create db
-    engine.execute("USE "+db_name)
-    data = read_csv(filename)
-    data.to_sql(tableName, engine, if_exists='append')
-
+    try:
+        loggerObject.logger.info("******** to_mysql starts")
+        global engine
+        tableName = filename[:-4]
+        engine.execute("CREATE DATABASE IF NOT EXISTS "+ db_name)  # create db
+        engine.execute("USE "+db_name)
+        data = read_csv(filename)
+        data.to_sql(tableName, engine, if_exists='append')
+    except Exception as e:
+        loggerObject.logger.info(e, exc_info = True)
+    loggerObject.logger.info("********** to_mysql ends")
 
 def from_mysql(db_name):
     """
     :param db_name:
     :return:
     """
-    global engine
-    engine.execute('USE '+ db_name)
-    tables = engine.execute('SHOW TABLES')
-    available_tables = tables.fetchall()
-    for i in range(len(available_tables)):
-       to_Json(available_tables[i][0], 'select * from ' + available_tables[i][0])
-       #to_Csv(available_tables[i][0], 'select * from ' + available_tables[i][0])
-
+    try:
+        loggerObject.logger.info("*************** from_mysql starts")
+        global engine
+        engine.execute('USE '+ db_name)
+        tables = engine.execute('SHOW TABLES')
+        available_tables = tables.fetchall()
+        for i in range(len(available_tables)):
+           to_Json(available_tables[i][0], 'select * from ' + available_tables[i][0])
+           #to_Csv(available_tables[i][0], 'select * from ' + available_tables[i][0])
+    except Exception as e:
+        loggerObject.logger.info(e, exc_info = True)
+    loggerObject.logger.info("************* from_mysql ends")
 
 def to_Json(filename, query):
     """
@@ -47,11 +58,15 @@ def to_Json(filename, query):
     :param query:
     :return:
     """
-    global engine
-    df = pd.read_sql(query, con=engine)
-    #df.to_json(orient='records')[1:-1].replace('},{', '} {')
-    df.to_json(filename+'.json', orient='records')
-
+    try:
+        loggerObject.logger.info("********* to_Json starts")
+        global engine
+        df = pd.read_sql(query, con=engine)
+        #df.to_json(orient='records')[1:-1].replace('},{', '} {')
+        df.to_json(filename+'.json', orient='records')
+    except Exception as e:
+        loggerObject.logger.info(e, exc_info  = True)
+    loggerObject.logger.info("******** to_Json ends")
 
 def to_Csv(filename, query):
     """
@@ -59,10 +74,15 @@ def to_Csv(filename, query):
     :param query:
     :return:
     """
-    df = pd.read_sql(query, con=engine)
-    df.to_csv(filename+'.csv', header= False)
-    #with open(db_name+'.csv', 'a') as f:
-    #    df.to_csv(f, header=False)
+    try:
+        loggerObject.logger.info("********* to_Csv starts")
+        df = pd.read_sql(query, con=engine)
+        df.to_csv(filename+'.csv', header= False)
+        #with open(db_name+'.csv', 'a') as f:
+        #    df.to_csv(f, header=False)
+    except Exception as e:
+        loggerObject.logger.info(e, exc_info  = True)
+    loggerObject.logger.info("********* to_Csv ends")
 
 
 if __name__=='__main__':
